c4fdb54cd068ce3ab68470a6ebb56f653a54791c
郭奕增 <guoyizeng@guoyizengdeMac.local>
2017-06-01 16:45:29 +0800

    1

---
+++ Identification/Identification/Model/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  Identification
+//
+//  Created by 郭奕增 on 2017/6/1.
+//  Copyright © 2017年 郭奕增. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
---
+++ Identification/Identification/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Identification
+//
+//  Created by 郭奕增 on 2017/6/1.
+//  Copyright © 2017年 郭奕增. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
